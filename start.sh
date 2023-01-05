18ec6802de7ca64856c850768611c10770a8ef59
Renishbot <117950358+Renishbot@users.noreply.github.com>
2023-01-05 21:49:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Millie/millie /millie
+  git clone https://github.com/Renishbot/millie-power.git /millie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /millie
 fi
 cd /millie
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🍁"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜. Millie ...🍁"
 python3 millie.py
